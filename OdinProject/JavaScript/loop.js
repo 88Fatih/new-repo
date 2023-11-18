eb33f2748f681bfe683a9dfb39cd5fd7bfbc5011
Phatih88 <mambet.fatih@gmail.com>
2023-11-18 15:05:56 +0200

    Initial commit

---
+++ OdinProject/JavaScript/loop.js
@@ -0,0 +1,4 @@
+let num
+do {
+    num = prompt("Enter a greater than 100?", 0);
+} while (num <= 100 && num);
---
+++ OdinProject/JavaScript/test4.js
@@ -0,0 +1,13 @@
+10 + 7;
+9 * 8;
+60 % 3;
+const num1 = 10;
+const num2 = 50;
+9 * num1;
+num1 ** 3;
+num2 / num1;
+5 + 10 * 3;
+(num2 % 9) * num1;
+num2 + num1 / 8 + 2;
+
+// (num2 + num1) / (8 + 2); //
